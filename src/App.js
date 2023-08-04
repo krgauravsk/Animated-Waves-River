d18e09990ee3769cce452b9ca7676d7160d83a37
Kumar Gaurav <66477464+krgauravsk@users.noreply.github.com>
2023-08-04 20:18:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AnimatedWaves from "./AnimatedWaves";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <AnimatedWaves />
+    </div>
+  );
+}
